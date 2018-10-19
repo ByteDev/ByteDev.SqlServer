d3f25663a11677b8984f59e1952163047882b1d8
Paul Stanley <paul.stanley@asos.com>
2018-10-19 15:11:15 +0100

    Initial version

---
+++ src/ByteDev.SqlServer.IntTests/MsSqlServerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ByteDev.SqlServer.IntTests
+{
+    [TestFixture]
+    public class MsSqlServerTests
+    {
+        [Test]
+        public void WhenServerExists_ThenReturnTrue()
+        {
+            const string connString = "Data Source=.;Integrated Security=true;";
+
+            var result = MsSqlServer.Exists(connString);
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ src/ByteDev.SqlServer/MsSqlServer.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace ByteDev.SqlServer
+{
+    public class MsSqlServer
+    {
+        public static bool Exists(string connectionString)
+        {
+            var builder = new SqlConnectionStringBuilder(connectionString)
+            {
+                ConnectTimeout = 1
+            };
+
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    connection.Close();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
