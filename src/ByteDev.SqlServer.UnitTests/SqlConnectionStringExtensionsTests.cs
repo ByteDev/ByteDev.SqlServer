d504ef34a75d358c6d61edb0434138965b109843
Paul Stanley <paul.stanley@asos.com>
2018-10-23 15:33:21 +0100

    Added SqlConnectionStringExtensions

---
+++ src/ByteDev.SqlServer.UnitTests/SqlConnectionStringExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ByteDev.SqlServer.UnitTests
+{
+    [TestFixture]
+    public class SqlConnectionStringExtensionsTests
+    {
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("Server=tcp:My-Server.database.windows.net,1433;Initial Catalog=MyDb;", true)]
+        [TestCase("server=tcp:My-Server.database.windows.net,1433;Initial Catalog=MyDb;", true)]
+        [TestCase("Data Source=tcp:My-Server.database.windows.net,1433;Initial Catalog=MyDb;", true)]
+        [TestCase("Initial Catalog=MyDb;Data Source=tcp:My-Server.database.windows.net,1433", true)]
+        public void WhenConnectionStringProvided_ThenReturnExpected(string value, bool expected)
+        {
+            var result = value.IsAzureConnectionString();
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ src/ByteDev.SqlServer/SqlConnectionStringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ByteDev.SqlServer
+{
+    public static class SqlConnectionStringExtensions
+    {
+        public static bool IsAzureConnectionString(this string source)
+        {
+            if(string.IsNullOrEmpty(source))
+                return false;
+
+            var regEx = new Regex(@"(Server|Data Source)=tcp:[a-zA-Z0-9-]*.database.windows.net", RegexOptions.IgnoreCase);
+
+            return regEx.IsMatch(source);
+        }
+    }
+}
